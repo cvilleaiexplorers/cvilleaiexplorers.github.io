60d02a7a80832f4a76d9cba0c276457576992ffe
roadfoodr <mpowers@alum.mit.edu>
2025-02-15 07:23:29 +0000

    Deploying to gh-pages from @ cvilleaiexplorers/cville-ai-explorers-website@66cb9d816d7886f2bf13d0d98ebfef3220212ad5 🚀

--- _next/static/z50yI5aEcexlGtk3kGcSJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/247-a677e7cb5ee9f306.js","static/chunks/pages/index-7f4509ef1c7ab0f6.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
