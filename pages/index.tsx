dba759d5c4f34e3fe0248fb5cbd53f578f7fbc0b
M Powers <mpowers@alum.mit.edu>
2025-03-24 22:26:32 -0400

    Update April meeting link

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import SponsorsCard from "../components/sponsors-card"
 import { useEffect } from "react"
 
 export default function Home() {
-  const nextMeetupDate = "March 12, 2025"
+  const nextMeetupDate = "April 22, 2025"
 
   useEffect(() => {
     const smoothScroll = (e: Event) => {
@@ -43,7 +43,7 @@ export default function Home() {
           <p className="text-2xl font-semibold text-blue-400">
             Next Meetup:{" "}
             <a
-              href="https://www.meetup.com/cville-tech/events/306249129/"
+              href="https://www.meetup.com/cville-tech/events/306883862/"
               className="underline hover:text-blue-300 transition-colors"
               target="_blank"
               rel="noopener noreferrer"
