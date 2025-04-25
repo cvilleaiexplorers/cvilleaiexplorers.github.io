a302beabfed6a715cd1c4c944a678a49ecc36fad
roadfoodr <mpowers@alum.mit.edu>
2025-04-25 12:50:47 +0000

    Deploying to gh-pages from @ cvilleaiexplorers/cvilleaiexplorers.github.io@8f341d61e4727f5d8c7e94448e24630e58f3ed36 🚀

---
+++ _next/static/23us113OOiivTB0lQ68nw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/996-b58ba31552ee7880.js","static/chunks/pages/index-5c57bf5a9fa40478.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/sF2wg_R3U46LGpOjl7D-i/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/996-b58ba31552ee7880.js","static/chunks/pages/index-1a4e5f4e86b6d66a.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
